f81958386b6177b0ff74612f529d120c79c75e73
Vladimir <sciphilib@gmail.com>
2023-12-24 02:48:11 +0700

    Fixed UI alignment

--- src/main/scala/ui/MyListApp.scala
+++ src/main/scala/ui/MyListApp.scala
@@ -4,30 +4,67 @@ import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ComboBox, ListView, TextField}
 import scalafx.scene.layout.VBox
 import scalafx.collections.ObservableBuffer
+import scalafx.geometry.Insets
 
 object MyListApp extends JFXApp3 {
   override def start(): Unit = {
     stage = new JFXApp3.PrimaryStage {
       title = "MyList Manager"
+      width = 800
+      height = 600
+
       scene = new Scene {
-        content = new VBox {
-          val comboBox = new ComboBox(Seq("Int", "Point2D"))
-          val listView = new ListView[String]()
-          val inputField = new TextField()
-          val addButton = new Button("Add")
-          val addAtIndexButton = new Button("Add at Index")
-          val deleteButton = new Button("Delete")
-          val deleteAtIndexButton = new Button("Delete at Index")
-          val indexField = new TextField()
+        val contentBox = new VBox {
+          padding = Insets(50)
+          spacing = 10
+          fillWidth = true
+          val comboBox = new ComboBox(Seq("Int", "Point2D")) {
+            maxWidth = Double.MaxValue
+            promptText = "Select Data Type"
+          }
+          val listView = new ListView[String]() { maxWidth = Double.MaxValue }
+          val inputField = new TextField() { maxWidth = Double.MaxValue }
+          val addButton = new Button("Add") { maxWidth = Double.MaxValue }
+          val addAtIndexButton = new Button("Add at Index") {
+            maxWidth = Double.MaxValue
+          }
+          val deleteButton = new Button("Delete") { maxWidth = Double.MaxValue }
+          val deleteAtIndexButton = new Button("Delete at Index") {
+            maxWidth = Double.MaxValue
+          }
+          val indexField = new TextField() { maxWidth = Double.MaxValue }
+
+          addButton.disable = true
+          addAtIndexButton.disable = true
+          deleteButton.disable = true
+          deleteAtIndexButton.disable = true
+
+          comboBox.selectionModel().selectedItem.onChange { (_, _, newValue) =>
+            val isTypeSelected = newValue != null
+            addButton.disable = !isTypeSelected
+            addAtIndexButton.disable = !isTypeSelected
+            deleteButton.disable = !isTypeSelected
+            deleteAtIndexButton.disable = !isTypeSelected
+          }
 
           children = Seq(
             comboBox,
             listView,
             new VBox {
+              spacing = 5
+              fillWidth = true
               children = Seq(
                 inputField,
                 new VBox {
-                  children = Seq(addButton, indexField, addAtIndexButton, deleteButton, deleteAtIndexButton)
+                  spacing = 5
+                  fillWidth = true
+                  children = Seq(
+                    addButton,
+                    indexField,
+                    addAtIndexButton,
+                    deleteButton,
+                    deleteAtIndexButton
+                  )
                 }
               )
             }
@@ -35,7 +72,7 @@ object MyListApp extends JFXApp3 {
 
           addButton.onAction = _ => {
             val item = comboBox.value.value match {
-              case "Int" => inputField.text.value
+              case "Int"     => inputField.text.value
               case "Point2D" => inputField.text.value
             }
             listView.items.value += item
@@ -44,7 +81,7 @@ object MyListApp extends JFXApp3 {
           addAtIndexButton.onAction = _ => {
             val idx = indexField.text.value.toIntOption.getOrElse(0)
             val item = comboBox.value.value match {
-              case "Int" => inputField.text.value
+              case "Int"     => inputField.text.value
               case "Point2D" => inputField.text.value
             }
             if (idx >= 0 && idx <= listView.items.value.size) {
@@ -65,6 +102,25 @@ object MyListApp extends JFXApp3 {
               listView.items.value.remove(idx)
             }
           }
+        }
+        root = contentBox
+
+        width.onChange { (_, _, newWidth) =>
+          contentBox.padding = Insets(
+            height() * 0.25,
+            newWidth.doubleValue() * 0.25,
+            height() * 0.25,
+            newWidth.doubleValue() * 0.25
+          )
+        }
+
+        height.onChange { (_, _, newHeight) =>
+          contentBox.padding = Insets(
+            newHeight.doubleValue() * 0.25,
+            width() * 0.25,
+            newHeight.doubleValue() * 0.25,
+            width() * 0.25
+          )
         }
       }
     }
